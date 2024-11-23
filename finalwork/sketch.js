6a2961a8318e3cca3d833fa41e847976b6775213
Imai0506 <2341041h@stu.kobe-u.ac.jp>
2024-11-24 00:10:29 +0900

    Finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,35 @@
-// 最終課題を制作しよう
+// キューバ国旗
+function setup() {
+  createCanvas(300, 150);
+  noStroke();
 
-function setup(){
-  createCanvas(windowWidth, windowHeight);
+  // 青と白のストライプを描く
+  for (let i = 0; i < 5; i++) {
+    if (i % 2 == 0) {
+      fill(0, 51, 153); // 青
+    } else {
+      fill(255); // 白
+    }
+    rect(0, (height / 5) * i, width, height / 5);
+  }
+
+  // 赤い三角形を描く
+  fill(206, 17, 38); // 赤
+  triangle(0, 0, 0, height, height / 2 * 1.73, height / 2);
+
+  // 白い星を描く
+  fill(255); // 白
+  drawStar(height / 3.5, height / 2, 20, 5); // 星を描画
 }
 
-function draw(){
-  background(160, 192, 255);
-}
-
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+// 星を描く関数
+function drawStar(cx, cy, r, n) {
+  beginShape();
+  for (let i = 0; i < n * 2; i++) {
+    let theta = PI / n * i - HALF_PI;
+    let x = cx + cos(theta) * (i % 2 === 0 ? r : r / 2);
+    let y = cy + sin(theta) * (i % 2 === 0 ? r : r / 2);
+    vertex(x, y);
+  }
+  endShape(CLOSE);
 }
