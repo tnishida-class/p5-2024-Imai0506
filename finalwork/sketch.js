49f795c0b459afd8c53bafa7feb24d691ab552c0
Imai0506 <2341041h@stu.kobe-u.ac.jp>
2024-11-26 10:49:43 +0900

    Finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,28 +1,38 @@
 // キューバ国旗　最終課題
-function setup() {
-  createCanvas(300, 150);
+let flagX = 0; 
+let flagY = 0; 
+
+function setup() { 
+  createCanvas(400, 300); 
   noStroke();
-
-  // 青と白のストライプを描く
-  for (let i = 0; i < 5; i++) {
-    if (i % 2 == 0) {
-      fill(0, 51, 153); // 青
-    } else {
-      fill(255); // 白
-    }
-    rect(0, (height / 5) * i, width, height / 5);
-  }
-
-  // 赤い三角形を描く
-  fill(206, 17, 38); // 赤
-  triangle(0, 0, 0, height, height / 2 * 1.73, height / 2);
-
-  // 白い星を描く
-  fill(255); // 白
-  drawStar(height / 3.5, height / 2, 20, 5); 
 }
 
-// 星を描く関数
+function draw() {
+  background(220); 
+  
+  flagX = mouseX - 150; 
+  flagY = mouseY - 75;
+
+  for (let i = 0; i < 5; i++) {
+    if (i % 2 == 0) {
+      fill(0, 51, 153); 
+    } else {
+      fill(255);
+    }
+    rect(flagX, flagY + (150 / 5) * i, 300, 150 / 5);
+  }
+
+  fill(206, 17, 38); 
+  triangle(
+    flagX, flagY, 
+    flagX, flagY + 150, 
+    flagX + 150 / 2 * 1.73, flagY + 150 / 2
+  );
+
+  fill(255); 
+  drawStar(flagX + 150 / 3.5, flagY + 150 / 2, 20, 5); 
+}
+
 function drawStar(cx, cy, r, n) {
   beginShape();
   for (let i = 0; i < n * 2; i++) {
